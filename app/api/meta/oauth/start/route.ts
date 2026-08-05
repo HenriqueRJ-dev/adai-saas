b899ff85aacbe5ba8de3d088bca034e813db1f59
HenriqueRJ-dev <contatohenriquerj@gmail.com>
2026-08-04 23:21:58 -0300

    Remove 'instagram_business_basic' from META_SCOPES

--- app/api/meta/oauth/start/route.ts
+++ app/api/meta/oauth/start/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 const META_SCOPES = [
   "ads_management",
   "pages_show_list",
-  "instagram_business_basic",
   "pages_read_engagement",
   "business_management",
 ].join(",");
